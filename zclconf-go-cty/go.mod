e62f3bf26dfa2b91c8a22d3b4c0c9ccd8e20feb6
Denis O <denis.o@linux.com>
2023-09-11 11:51:37 -0700

    Add test for go-cty

--- zclconf-go-cty/go.mod
+++ zclconf-go-cty/go.mod
@@ -1,3 +1,10 @@
 module github.com/denis256/go-tests/zclconf-go-cty
 
 go 1.18
+
+require github.com/zclconf/go-cty v1.14.0
+
+require (
+	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
+)
--- zclconf-go-cty/main.go
+++ zclconf-go-cty/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/gocty"
+	ctyjson "github.com/zclconf/go-cty/cty/json"
+
+	"log"
+)
+
 func main() {
 
+	valMap := map[string]cty.Value{}
+	valMap["test"] = cty.NullVal(cty.DynamicPseudoType)
+	valMap["test2"] = cty.UnknownVal(cty.DynamicPseudoType)
+
+	valMapAsCty, err := gocty.ToCtyValue(valMap, generateTypeFromValuesMap(valMap))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonBytes, err := ctyjson.Marshal(valMapAsCty, cty.DynamicPseudoType)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(jsonBytes))
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(jsonBytes, &result); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("%#v\n", result)
+}
+
+func generateTypeFromValuesMap(valMap map[string]cty.Value) cty.Type {
+	outType := map[string]cty.Type{}
+	for k, v := range valMap {
+		outType[k] = v.Type()
+	}
+	return cty.Object(outType)
 }
